2b4ff2692040c943851cb81e8e1a1b54e15b45b8
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-25 16:32:28 -0500

    change

--- js/script.js
+++ js/script.js
@@ -17,11 +17,6 @@ if (navigator.serviceWorker) {
 
 const randomNumber = Math.floor(Math.random() * 6) + 1
 
-function myButtonClicked() {
-  document.getElementById("answer").innerHTML =
-    "<p>Value is: " + slider.value + "</p>"
-}
-
 function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
 
